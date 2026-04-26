8b690eaead1c119041a3fd0a96b81f4c91911607
kenya-sagara <sagara@sagbrain.com>
2026-04-26 20:30:12 +0900

    独自ドメイン sake-jazz.artistmerge.jp で公開するよう設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://sake-jazz-net.github.io",
+  site: "https://sake-jazz.artistmerge.jp",
   output: "static",
   integrations: [sitemap()],
 });
